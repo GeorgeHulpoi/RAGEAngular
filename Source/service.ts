ef7d5dcdfc1c7fe8d2819769590d61cd40cef1e1
Armyw0w <armywwpt@gmail.com>
2017-09-05 19:51:28 +0300

    Update 1.0.2
    Added events
       - Call from index.html to Angular
       - Call from Angular to index.html (forward to client)

--- Source/service.ts
+++ Source/service.ts
@@ -1,15 +1,42 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 declare var window: any;
 
 @Injectable()
 export class RAGE {
+    private CustomEvents: EventEmitter<RAGEEvent> = new EventEmitter<RAGEEvent>();
 
     constructor() {
-		window.RAGE = {};
+		window.RAGE = {
+            call: this.call,
+        };
 
         if ( typeof window.RAGEInit === 'function') {
-		     window.RAGEInit();
+		    window.RAGEInit();
+        }
+        else if ( typeof window.RLoaded === 'boolean' ) {
+            window.RLoaded = true;
         }
     }
+
+    private call: Function = (func: string | number, ...args: any[]): void  => {
+        this.CustomEvents.emit({
+            func: func,
+            args: args
+        });
+    }
+
+    get listen(): EventEmitter<RAGEEvent> {
+        return this.CustomEvents;
+    }
+
+    callClient(func: string | number, ...args: any []): void  {
+        if ( typeof window.callClient !== 'function') throw 'callClient isn\'t set in index.html';
+        window.callClient(func,args);
+    }
+}
+
+export interface RAGEEvent {
+    func: string | number;
+    args: any[];
 }
